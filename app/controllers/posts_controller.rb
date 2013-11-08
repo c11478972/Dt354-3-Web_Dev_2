4c8483b8490fc90bb00ad4ebd344a4727da6f5ce
c11478972 <c11478972@mydit.ie>
2013-11-08 10:50:40 +0000

    This is the first commit of Movieapp from h:

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,14 @@
+class PostsController < ApplicationController
+	before_filter :authorise
+	def create
+		@movie = Movie.find params[:movie_id]
+		if @movie
+			@post = @movie.posts.create params[:post]
+			@post.user_id = @current_user.id
+			@post.save
+		end
+		respond_to do |format|
+			format.html { redirect_to @movie}
+		end
+	end
+end
---
+++ app/models/movie.rb
@@ -0,0 +1,12 @@
+class Movie < ActiveRecord::Base
+	belongs_to :genre
+	has_many :posts, :dependent => :destroy
+	validates :title, presence: true,  uniqueness: true
+	validate :must_have_vaild_trailer
+	
+	def must_have_vaild_trailer
+		unless trailer.include?("<iframe")
+			errors.add(:trailer, "Must include an iframe tag")
+		end
+	end
+end
