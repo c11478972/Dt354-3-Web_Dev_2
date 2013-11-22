382ff27ce68e3d6aa13cac22d396d306b2d1ec90
c11478972 <c11478972@mydit.ie>
2013-11-22 11:59:48 +0000

    JS update comment

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,11 +2,19 @@ class PostsController < ApplicationController
 	before_filter :authorise
 	def create
 		@movie = Movie.find params[:movie_id]
-		if @movie
-			@post = @movie.posts.create params[:post]
-			@post.user_id = @current_user.id
-			@post.save
+		@post = @movie.posts.create params[:post]
+		@post.user_id = @current_user.id
+		@post.save
+		respond_to do |format|
+			format.js #create.js.erb
+			format.html { redirect_to @movie}
 		end
+	end
+	
+	def destroy
+		@movie = Movie.find(params[:movie_id])
+		@post = Post.find(params[:id])
+		@post.destroy
 		respond_to do |format|
 			format.html { redirect_to @movie}
 		end
